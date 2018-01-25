a1f2319e4e020c659eb04557409b97319fdbfcf6
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2018-01-25 09:45:55 +0100

    programa completado

--- src/añadirlibreriacod/AñadirLibreriaCOD.java
+++ src/añadirlibreriacod/AñadirLibreriaCOD.java
@@ -1,10 +1,12 @@
 package añadirlibreriacod;
+import com.otros.milibreria.MisMetodos;
 /**
  * @author Juan Borrajo Rodriguez Nº5937
  */
 public class AñadirLibreriaCOD {
 
     public static void main(String[] args) {
+        MisMetodos.unMetodo(12);
 
     }
 
